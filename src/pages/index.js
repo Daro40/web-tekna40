6ba874ec81dec8945b0c2650863389729aba11ca
Daro40 <daroforti@gmail.com>
2021-01-30 15:51:25 -0300

    test actions

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from "react"
 
 export default function Home() {
   return <div style={{ color: `purple`}}>
-    <h1>keloke! cumbia 420 para los negrosss</h1>
-    <p>fucking shit</p>
+    <h1>keloke! cumbia 420 para los negro</h1>
+    <p>f*cking shit</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
 }
