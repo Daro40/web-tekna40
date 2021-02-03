ebebdfe55f7ba74ab629b9421efbe3a7e4eb8c6e
Daro40 <daroforti@gmail.com>
2021-02-03 15:34:09 -0300

    123 probando

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,8 @@ import React from "react"
 
 export default function Home() {
   return <div style={{ color: `purple`}}>
-    <h1>keloke! cumbia 420 para los negro</h1>
+    <h1>keloke! cumbia 420 para l4s negro</h1>
+    <h2>123 probando</h2>
     <p>f*cking shit</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
     </div>
