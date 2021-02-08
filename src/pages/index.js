4be53d22ac5e5d00bdb61920860c296e6458b2c0
Daro40 <daroforti@gmail.com>
2021-02-08 11:38:01 -0300

    cambio de titulo

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function Home() {
   return <div style={{ color: `purple`}}>
-    <h1>keloke! cumbia 420 para l4s negro</h1>
+    <h1>WEB DE EMPRESA TEKNA40</h1>
     <h2>123 probando</h2>
     <p>f*cking shit</p>
     <img src="https://source.unsplash.com/random/400x200" alt="" />
